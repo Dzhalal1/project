4ad1578a588a36fd59b53537850f0be95039e32c
Dzhalal <12345@mail.ru>
2020-09-29 11:13:58 +0300

    Vue tut practise1

--- Vue/src/App.vue
+++ Vue/src/App.vue
@@ -1,72 +1,88 @@
 <template>
   <v-app>
     <v-navigation-drawer
+      color="yellow accent-4"
       app
       temporary
       v-model="drawer"
     >
       <v-card
+        color="black"
         class="mx-auto"
         width="300"
       >
-        <v-list>
+        <v-list color="yellow accent-4">
+
           <v-list-item @click="">
             <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
+              <v-icon color="black">mdi-home</v-icon>
             </v-list-item-icon>
             <v-list-item-title>Home</v-list-item-title>
           </v-list-item>
+
           <v-list-item @click="">
             <v-list-item-icon>
-              <v-icon>mdi-lock</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Login</v-list-item-title>
-          </v-list-item>
-          <v-list-item @click="">
-            <v-list-item-icon>
-              <v-icon>mdi-message-text</v-icon>
+              <v-icon color="black">mdi-message-text</v-icon>
             </v-list-item-icon>
             <v-list-item-title>Ask</v-list-item-title>
           </v-list-item>
+
+          <v-list-item @click="">
+            <v-list-item-icon>
+              <v-icon color="black">mdi-account-plus</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Registration</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item @click="">
+            <v-list-item-icon>
+              <v-icon color="black">mdi-login</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Login</v-list-item-title>
+          </v-list-item>
+
         </v-list>
       </v-card>
     </v-navigation-drawer>
+
     <v-card
-      color="grey lighten-4"
+      color="yellow accent-4"
       flat
-      height="200px"
+      height="935px"
       tile
     >
-      <v-toolbar dark>
-        <v-app-bar-nav-icon @click="drawer = !drawer" class="dropdown-menu-md-left"></v-app-bar-nav-icon>
-
-        <v-toolbar-title>Mercedes-Benz</v-toolbar-title>
+      <v-toolbar color="yellow accent-4">
+        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-toolbar-title></v-toolbar-title>
+        <img src="../src/assets/1280px-Commerzbank_(2009).svg.png" height="60" width="250"/>
 
         <v-spacer></v-spacer>
-
         <v-btn icon>
-          <v-icon>mdi-magnify</v-icon>
+          <v-icon color="black">mdi-home</v-icon>
         </v-btn>
         <v-btn icon>
-          <v-icon>mdi-account-plus</v-icon>
+          <v-icon color="black">mdi-message-text</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon color="black">mdi-account-plus</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon color="black">mdi-login</v-icon>
         </v-btn>
       </v-toolbar>
+
+
+
     </v-card>
 
-    <div style="background-color: red" class="d-md-none">
-      <button>ffgfgf</button>
-    </div>
 
   </v-app>
 </template>
-
-
 <script>
   export default {
     data() {
       return {
         drawer: false
-
 
       }
     }
